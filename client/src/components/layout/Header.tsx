86c9993229b7c3ffed05124833c3c2e27e8c1666
Alex Martishin <asmartishin@gmail.com>
2025-02-25 00:41:58 +0100

    Change website name

--- client/src/components/layout/Header.tsx
+++ client/src/components/layout/Header.tsx
@@ -27,7 +27,7 @@ export default function Header({ userDetails, setUserDetails }: HeaderProps): JS
     <div className="flex flex-wrap items-center">
       <div className="max-w-full flex-1 flex-grow px-4">
         <Link to="/">
-          <h1 className="text-3xl font-bold tracking-tight">Go watch a movie!</h1>
+          <h1 className="text-3xl font-bold tracking-tight">MovieSearch</h1>
         </Link>
       </div>
       <div className="max-w-full flex-grow px-4 text-end">
